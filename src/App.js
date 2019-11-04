866b0bf1126c6b9f05ddff9ffa98aa9f303c22bd
Tiago Pádua <tiagolpadua@gmail.com>
2019-11-03 21:47:51 -0300

    c4

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component, Fragment } from "react";
+import Cabecalho from "./components/Cabecalho";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Cabecalho usuario="@omariosouto" />
+        Resto da página que ainda iremos adicionar
+      </Fragment>
+    );
+  }
 }
 
 export default App;
