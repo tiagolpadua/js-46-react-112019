07a26f0ff00c2c1dd6b4c7665f0b19f45f74d159
Tiago Lage Payne de Padua <tiagolpadua@gmail.com>
2019-11-04 23:48:34 -0200

    c12

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-// CSSs Globais
+// CSS Global
 import "./assets/css/reset.css";
 import "./assets/css/container.css";
 import "./assets/css/btn.css";
 import "./assets/css/icon.css";
 import "./assets/css/iconHeart.css";
 import "./assets/css/notificacao.css";
-
 import "./assets/css/novoTweet.css";
 // import './index.css';
-
-import App from "./App";
+// import HomePage from "./pages/HomePage";
 import * as serviceWorker from "./serviceWorker";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-
+// Roteamento
+import { BrowserRouter } from "react-router-dom";
+import Roteamento from "./routes.js";
+ReactDOM.render(
+  <BrowserRouter>
+    <Roteamento />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
