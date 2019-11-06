d3de6f55cf84a5229aeafc47471cea0e0d46ad83
Tiago Lage Payne de Padua <tiagolpadua@gmail.com>
2019-11-06 00:08:47 -0200

    c15

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,33 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 //	Páginas
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+
+class PrivateRoute extends React.Component {
+  estaAutenticado = () => {
+    if (localStorage.getItem("TOKEN")) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  render() {
+    const { component: Component, ...props } = this.props;
+    if (this.estaAutenticado()) {
+      return <Component {...props} />;
+    } else {
+      return <Redirect to="/login" />;
+    }
+  }
+}
+
 class Roteamento extends React.Component {
   render() {
     return (
       <Switch>
-        <Route path="/" component={HomePage} exact />
+        <PrivateRoute path="/" component={HomePage} exact />
         <Route path="/login" component={LoginPage} />
       </Switch>
     );
